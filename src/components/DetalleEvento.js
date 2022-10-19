9d06c25860315aad17faa7e2eba8a4c044c743bf
luisrza <werox51@gmail.com>
2022-10-18 19:36:34 -0500

    cambios dashboard

--- src/components/DetalleEvento.js
+++ src/components/DetalleEvento.js
@@ -22,17 +22,22 @@ useEffect(()=>{
 
 
 return (
-<div>
-  <h1>Detalles del evento...!</h1>
-  <h2>Ciudad: {evento.city}</h2>
-  <h2>Pais: {evento.country}</h2>
-  <h2>Precio: {evento.currency}  {evento.price}</h2>
-  <h2>Fecha: {evento.date}</h2>
-  <h2>Evento Disponible: {evento.agotado===0?'Si':'Lo siento... se encuentra agotado este evento!!'}</h2>
-  <h3>Description: {evento.description}</h3>
+<div className="detalles-container">
+  <h1 className="h1-city">Ciudad: {evento.city}</h1>
+  <div className="img-container">
+    <img className="detalle-img" src={`http://localhost:4000/img/${evento.image}`}></img>
+   </div>
+  <div className="info-container">
+    <h2>Detalles del evento:</h2>
+    <h2>Pais: {evento.country}</h2>
+    <h2>Precio: {evento.currency}  {evento.price}</h2>
+    <h2>Fecha: {evento.date}</h2>
+    <h2>Evento Disponible: {evento.agotado===0?'Si':'Lo siento... se encuentra agotado este evento!!'}</h2>
+    <h3>Description: {evento.description}</h3>
+  </div>
 
 
-  <img src={`http://localhost:4000/img/${evento.image}`}></img>
+
 
 </div>
 )  
